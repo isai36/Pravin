293dd839178b65334cd58e6c5d4ae483e40a5738
Daily Lim <dailylim1995@gmail.com>
2025-03-26 17:08:00 +0800

    Added of .pythong-version

--- Pravin/settings.py
+++ Pravin/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://pravin-sharedoc-b7b33e451814.herokuapp.com/", "127.0.0.1"]
+ALLOWED_HOSTS = ["pravin-sharedoc-b7b33e451814.herokuapp.com/", "127.0.0.1"]
 
 # Application definition
 
